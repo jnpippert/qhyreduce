23053bb1c1123756b74df559fdc0d477da97a781
jnpippert <pippertjanniklas@gmail.com>
2024-11-07 13:32:18 +0100

    added script

--- 40autoreduce.sh
+++ 40autoreduce.sh
@@ -1,2 +1,16 @@
 #!usr/bin/bash
 
+SUBMIT=/data/wst/u/jpippert/qhyreduce/scripts/submit_job.sh
+REDUCE=/data/wst/u/jpippert/qhyreduce/40autoreduce.py
+help () {
+    echo "TODO Help"
+    exit 0
+}
+
+
+#if [ "$#" -eq 0 ] || [ "$1" == "h" ];  then
+#    help;
+#fi
+
+echo $SUBMIT python $REDUCE $*
+
