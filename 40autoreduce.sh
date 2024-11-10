5c73d69a8a48cf0b5561c5e33a8ce32a4db7d69b
Jan-Niklas Pippert <jan-niklas.pippert@online.de>
2024-11-10 19:23:23 +0100

    updated 40*.sh

--- 40autoreduce.sh
+++ 40autoreduce.sh
@@ -1,16 +1,54 @@
 #!usr/bin/bash
 
-SUBMIT=/data/wst/u/jpippert/qhyreduce/scripts/submit_job.sh
-REDUCE=/data/wst/u/jpippert/qhyreduce/40autoreduce.py
+# PATHS
+PYREDUCE=/data/wst/u/jpippert/qhyreduce/40autoreduce.py
+
+# VARIABLES
+CENTERTYPE="ALL"
+IMAGEOUTNAME="coadd"
+NTHREADS=0
+NPROC=8
+AUTOREDUCE=""
+
+# METHODS
 help () {
-    $REDUCE -h
+    $PYREDUCE -h
     exit 0
 }
 
-
+# SCRIPT START
 if [ "$#" -eq 0 ] || [ "$1" == "h" ];  then
     help;
 fi
 
-qsub $SUBMIT python $REDUCE $*
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -h | --help)
+            help
+            exit
+            ;;
+
+        # SWARP KWARGS
+        -CENTER)
+            CENTER="$2"
+            shift 2
+            ;;
+        -IMAGEOUT_NAME)
+            IMAGEOUTNAME="$2"
+            shift 2
+            ;;
+        -NTHREADS)
+            NTHREADS="$2"
+            shift 2;
+            ;;
+        *)
+            AUTOREDUCE=$AUTOREDUCE "$1"
+            shift 1
+            ;;
+    esac
+done
+
+echo $AUTOREDUCE
+
+
 
